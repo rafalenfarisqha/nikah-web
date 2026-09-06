b37adec78dcc9ae22a36577e2f7e005ecd6ce9a7
rafalenfarisqha <rafalen.labibsani.farisqha-2025@vokasi.unair.ac.id>
2026-09-06 11:08:57 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const mainInvitation = document.getElementById('main-invitation');
 const bgMusic = document.getElementById('bg-music');
 
 openBtn.addEventListener('click', () => {
-    // Sembunyikan halaman pertama, tampilkan undangan utama
     welcomeScreen.classList.add('hidden');
     mainInvitation.classList.remove('hidden');
 
-    // Putar musik latar belakang
     bgMusic.play().catch(error => {
         console.log("Autoplay dicegah oleh browser.");
     });
